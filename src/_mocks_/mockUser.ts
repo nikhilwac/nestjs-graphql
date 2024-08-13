66ebde257597fe4c3f5926d9af633b1fffe82a27
nikhil.b_wac_node <nikhil.b@webandcrafts.com>
2024-08-13 18:05:42 +0530

    first commit

---
+++ src/_mocks_/mockUser.ts
@@ -0,0 +1,22 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'nikhil',
+    displayname: 'nikhil the developer',
+  },
+  {
+    id: 2,
+    username: 'athul',
+    displayname: 'athul the developer',
+  },
+  {
+    id: 3,
+    username: 'aslam',
+    displayname: 'aslam the developer',
+  },
+  {
+    id: 4,
+    username: 'shilpa',
+    displayname: 'shilpa the developer',
+  },
+];
---
+++ src/graphql/resolvers/UserResolver.ts
@@ -0,0 +1,11 @@
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { User } from '../models/User';
+import { mockUsers } from 'src/_mocks_/mockUser';
+
+@Resolver()
+export class UserResolver {
+  @Query((returns) => User, { nullable: true })
+  getUserById(@Args('id', { type: () => Int }) id: number) {
+    return mockUsers.find((user) => user.id === id);
+  }
+}
